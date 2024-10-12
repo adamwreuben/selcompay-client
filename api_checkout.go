3233d551c97a389c43086e96fbe24684f64a3346
jkarage <josephbkarage@gmail.com>
2024-10-12 14:02:18 +0300

    fix: input types

--- api_checkout.go
+++ api_checkout.go
@@ -4,50 +4,48 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-
-	"github.com/google/uuid"
 )
 
 // OrderInput represents the inputfor the checkout order call.
 type OrderInput struct {
-	Vendor                string    `json:"vendor"`
-	ID                    uuid.UUID `json:"order_id"`
-	BuyerEmail            string    `json:"buyer_email"`
-	BuyerName             string    `json:"buyer_name"`
-	BuyerUserID           string    `json:"buyer_userid,omitempty"`
-	BuyerPhone            string    `json:"buyer_phone"`
-	GatewayBuyerUUID      string    `json:"gateway_buyer_uuid"`
-	Amount                int       `json:"amount"`
-	Currency              string    `json:"currency"`
-	PaymentMethods        string    `json:"payment_methods"`
-	RedirectURL           string    `json:"redirect_url,omitempty"`
-	CancelURL             string    `json:"cancel_url,omitempty"`
-	Webhook               string    `json:"webhook,omitempty"`
-	BillingFirstName      string    `json:"billing.firstname"`
-	BillingLastName       string    `json:"billing.lastname"`
-	BillingAddress1       string    `json:"billing.address_1"`
-	BillingAddress2       string    `json:"billing.address_2,omitempty"`
-	BillingCity           string    `json:"billing.city"`
-	BillingStateRegion    string    `json:"billing.state_or_region"`
-	BillingPostCodePOBox  string    `json:"billing.postcode_or_pobox"`
-	BillingCountry        string    `json:"billing.country"`
-	BillingPhone          string    `json:"billing.phone"`
-	ShippingFirstName     string    `json:"shipping.firstname,omitempty"`
-	ShippingLastName      string    `json:"shipping.lastname,omitempty"`
-	ShippingAddress1      string    `json:"shipping.address_1,omitempty"`
-	ShippingAddress2      string    `json:"shipping.address_2,omitempty"`
-	ShippingCity          string    `json:"shipping.city,omitempty"`
-	ShippingStateRegion   string    `json:"shipping.state_or_region,omitempty"`
-	ShippingPostCodePOBox string    `json:"shipping.postcode_or_pobox,omitempty"`
-	ShippingCountry       string    `json:"shipping.country,omitempty"`
-	ShippingPhone         string    `json:"shipping.phone,omitempty"`
-	BuyerRemarks          string    `json:"buyer_remarks,omitempty"`
-	MerchantRemarks       string    `json:"merchant_remarks,omitempty"`
-	NumberItems           int       `json:"no_of_items,omitempty"`
-	HeaderColour          string    `json:"header_colour,omitempty"`
-	LinkColour            string    `json:"link_colour,omitempty"`
-	ButtonColour          string    `json:"button_colour,omitempty"`
-	Expiry                int       `json:"expiry,omitempty"`
+	Vendor                string `json:"vendor"`
+	ID                    string `json:"order_id"`
+	BuyerEmail            string `json:"buyer_email"`
+	BuyerName             string `json:"buyer_name"`
+	BuyerUserID           string `json:"buyer_userid,omitempty"`
+	BuyerPhone            string `json:"buyer_phone"`
+	GatewayBuyerUUID      string `json:"gateway_buyer_uuid"`
+	Amount                int    `json:"amount"`
+	Currency              string `json:"currency"`
+	PaymentMethods        string `json:"payment_methods"`
+	RedirectURL           string `json:"redirect_url,omitempty"`
+	CancelURL             string `json:"cancel_url,omitempty"`
+	Webhook               string `json:"webhook,omitempty"`
+	BillingFirstName      string `json:"billing.firstname"`
+	BillingLastName       string `json:"billing.lastname"`
+	BillingAddress1       string `json:"billing.address_1"`
+	BillingAddress2       string `json:"billing.address_2,omitempty"`
+	BillingCity           string `json:"billing.city"`
+	BillingStateRegion    string `json:"billing.state_or_region"`
+	BillingPostCodePOBox  string `json:"billing.postcode_or_pobox"`
+	BillingCountry        string `json:"billing.country"`
+	BillingPhone          string `json:"billing.phone"`
+	ShippingFirstName     string `json:"shipping.firstname,omitempty"`
+	ShippingLastName      string `json:"shipping.lastname,omitempty"`
+	ShippingAddress1      string `json:"shipping.address_1,omitempty"`
+	ShippingAddress2      string `json:"shipping.address_2,omitempty"`
+	ShippingCity          string `json:"shipping.city,omitempty"`
+	ShippingStateRegion   string `json:"shipping.state_or_region,omitempty"`
+	ShippingPostCodePOBox string `json:"shipping.postcode_or_pobox,omitempty"`
+	ShippingCountry       string `json:"shipping.country,omitempty"`
+	ShippingPhone         string `json:"shipping.phone,omitempty"`
+	BuyerRemarks          string `json:"buyer_remarks,omitempty"`
+	MerchantRemarks       string `json:"merchant_remarks,omitempty"`
+	NumberItems           int    `json:"no_of_items,omitempty"`
+	HeaderColour          string `json:"header_colour,omitempty"`
+	LinkColour            string `json:"link_colour,omitempty"`
+	ButtonColour          string `json:"button_colour,omitempty"`
+	Expiry                int    `json:"expiry,omitempty"`
 }
 
 // CreateOrder creates a payment order request to the selcom payment gateway.
@@ -64,23 +62,23 @@ func (cln *Client) CreateOrder(ctx context.Context, order OrderInput) (Response,
 }
 
 type OrderInputMinimal struct {
-	Vendor          string    `json:"vendor"`
-	ID              uuid.UUID `json:"order_id"`
-	BuyerEmail      string    `json:"buyer_email"`
-	BuyerName       string    `json:"buyer_name"`
-	BuyerPhone      string    `json:"buyer_phone"`
-	Amount          int       `json:"amount"`
-	Currency        string    `json:"currency"`
-	RedirectURL     string    `json:"redirect_url,omitempty"`
-	CancelURL       string    `json:"cancel_url,omitempty"`
-	Webhook         string    `json:"webhook,omitempty"`
-	BuyerRemarks    string    `json:"buyer_remarks,omitempty"`
-	MerchantRemarks string    `json:"merchant_remarks,omitempty"`
-	NumberItems     int       `json:"no_of_items,omitempty"`
-	HeaderColour    string    `json:"header_colour,omitempty"`
-	LinkColour      string    `json:"link_colour,omitempty"`
-	ButtonColour    string    `json:"button_colour,omitempty"`
-	Expiry          int       `json:"expiry,omitempty"`
+	Vendor          string `json:"vendor"`
+	ID              string `json:"order_id"`
+	BuyerEmail      string `json:"buyer_email"`
+	BuyerName       string `json:"buyer_name"`
+	BuyerPhone      string `json:"buyer_phone"`
+	Amount          int    `json:"amount"`
+	Currency        string `json:"currency"`
+	RedirectURL     string `json:"redirect_url,omitempty"`
+	CancelURL       string `json:"cancel_url,omitempty"`
+	Webhook         string `json:"webhook,omitempty"`
+	BuyerRemarks    string `json:"buyer_remarks,omitempty"`
+	MerchantRemarks string `json:"merchant_remarks,omitempty"`
+	NumberItems     int    `json:"no_of_items,omitempty"`
+	HeaderColour    string `json:"header_colour,omitempty"`
+	LinkColour      string `json:"link_colour,omitempty"`
+	ButtonColour    string `json:"button_colour,omitempty"`
+	Expiry          int    `json:"expiry,omitempty"`
 }
 
 // CreateOrderMinimal creates a payment order request to the selcom payment gateway.
@@ -195,13 +193,13 @@ func (cln *Client) CardPayment(ctx context.Context, cardPaymentInput CardPayment
 
 // WalletPayment  api allows the ecommerce website to process an order using mobile wallets directly.
 // trigger this api call to reiceve a PUSH ussd.
-func (cln *Client) WalletPayment(ctx context.Context, transactionID uuid.UUID, orderID uuid.UUID, phone string) (Response, error) {
+func (cln *Client) WalletPayment(ctx context.Context, transactionID string, orderID string, phone string) (Response, error) {
 	url := fmt.Sprintf("%s/%s/checkout/wallet-payment", cln.host, version)
 
 	body := struct {
-		TransactionID uuid.UUID `json:"transid"`
-		OrderID       uuid.UUID `json:"order_id"`
-		MSISDN        string    `json:"msisdn"`
+		TransactionID string `json:"transid"`
+		OrderID       string `json:"order_id"`
+		MSISDN        string `json:"msisdn"`
 	}{
 		TransactionID: transactionID,
 		OrderID:       orderID,
